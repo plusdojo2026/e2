1653e10d93730a0c2e4414548324b905c4ac3b77
plusdojo2026-otaka <otaka-haruki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-12 17:47:27 +0900

    今日やったとこ

---
+++ source/src/main/java/dao/BudgetDao.java
@@ -0,0 +1,147 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dto.BudgetDto;
+
+public class BudgetDao {
+	public boolean insert(BudgetDto budget) {
+
+        Connection conn = null;
+        boolean result = false;
+
+        try {
+            // JDBCドライバ読み込み
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // DB接続
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/webapp1?"
+                + "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9",
+                "root",
+                "password"
+            );
+
+            // SQL
+            String sql = "INSERT INTO budgets (budget_amount, target_amount, user_id) "
+                  + "VALUES (?, ?, ?)";
+            PreparedStatement pStmt = conn.prepareStatement(sql);
+
+            // パラメータ設定
+            pStmt.setInt(1, budget.getBudget_amount());
+            pStmt.setInt(2, budget.getTarget_amount());
+            pStmt.setString(3, budget.getUser_id());
+
+            // 実行
+            if (pStmt.executeUpdate() == 1) {
+                result = true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            // DB切断
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return result;
+    }
+	
+	public boolean update(BudgetDto budget) {
+
+	    Connection conn = null;
+	    boolean result = false;
+
+	    try {
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+
+	        conn = DriverManager.getConnection(
+	            "jdbc:mysql://localhost:3306/webapp1?"
+	            + "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9",
+	            "root",
+	            "password"
+	        );
+
+	        String sql = "UPDATE budgets SET budget_amount = ?, target_amount = ? "
+	                + "WHERE user_id = ?";
+	        PreparedStatement pStmt = conn.prepareStatement(sql);
+
+	        pStmt.setInt(1, budget.getBudget_amount());
+            pStmt.setInt(2, budget.getTarget_amount());
+            pStmt.setString(3, budget.getUser_id());
+
+	        if (pStmt.executeUpdate() == 1) {
+	            result = true;
+	        }
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (conn != null) {
+	            try {
+	                conn.close();
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+
+	    return result;
+	}
+	
+	public BudgetDto select(String userId) {
+
+		 Connection conn = null;
+	     BudgetDto budget = null;
+
+	    try {
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+
+	        conn = DriverManager.getConnection(
+	            "jdbc:mysql://localhost:3306/webapp1?"
+	            + "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9",
+	            "root",
+	            "password"
+	        );
+
+	        String sql = "SELECT budget_amount, target_amount "
+	                + "FROM budgets WHERE user_id = ?";
+
+	        PreparedStatement pStmt = conn.prepareStatement(sql);
+	        pStmt.setString(1, userId);
+
+	        ResultSet rs = pStmt.executeQuery();
+	        
+	        if (rs.next()) {
+	        	budget = new BudgetDto();
+                budget.setBudget_amount(rs.getInt("budget_amount"));
+                budget.setTarget_amount(rs.getInt("target_amount"));
+                budget.setUser_id(userId);
+	        }
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	            if (conn != null) conn.close();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    return budget;
+	}
+}
+
