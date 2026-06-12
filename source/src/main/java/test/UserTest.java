0e07865a3f79725e5bb2f0c2e6bddddc8e1667ba
plusdojo2026-araki <araki-shunnosuke-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-12 09:09:57 +0900

    Update UserTest.java

--- source/src/main/java/test/UserTest.java
+++ source/src/main/java/test/UserTest.java
@@ -10,21 +10,21 @@ public class UserTest {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-        UserDao dao = new UserDao();
 
-        UserDto user = new UserDto();
-        user.setId("user1");
-        user.setPw("pass123");
-
-        boolean result = dao.isLoginOK(user);
-
-        if (result) {
-            System.out.println("ログイン成功");
-        } else {
-            System.out.println("ログイン失敗");
-        }
-    }
-
+	UserDao dao = new UserDao();
+	
+	UserDto user = new UserDto();
+	user.setId("testuser");
+	user.setPw("1234");
+	user.setName("テスト太郎");
+	
+	// ① 登録
+	boolean insertResult = dao.insert(user);
+	System.out.println(insertResult);
+	
+	// ② ログイン
+	boolean loginResult = dao.isLoginOK(user);
+	System.out.println(loginResult);
+		}
 }
-
 
