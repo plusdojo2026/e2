d2be12160f3e5b6b5ea2cc3fe94711ddb7232764
plusdojo2026-ouchihiroki <ouchi-hiroki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 13:03:34 +0900

    Expenses調整

--- source/src/main/java/dto/ExpensesDto.java
+++ source/src/main/java/dto/ExpensesDto.java
@@ -3,18 +3,18 @@ package dto;
 import java.io.Serializable;
 
 public class ExpensesDto implements Serializable {
-<<<<<<< HEAD
 	private Integer id; // 主キー
 	private String user_id; //ユーザーID
 	private Integer amount; //金額
 	private String emotion; // 感情
-	private String category; //カテゴリ
- 	private String situation; //状況
-	private String item_name; //商品名
-	private String memo; //　備考
-	private String created_at; //　日付
-	private String tag; //　タグ
+	private String category; // カテゴリ
+	private String situation; // 状況
+	private String item_name; // 商品名
+	private String memo; // 備考
+	private String created_at; // 日付
+	private String tag; // タグ
 
+	
 	//getterとsetter
 	public Integer getId() {
 		return id;
@@ -24,101 +24,95 @@ public class ExpensesDto implements Serializable {
 		this.id = id;
 	}
 
-=======
-	private String user_id; // ユーザーID
-	private Integer amount; // 金額
-	private String emotion; // 感情
-	private String category; // カテゴリ
-	private String situation; // 状況
-	private String item_name; // 商品名
-	private String memo; // 備考
-	private String created_at; // 日付
-	private String tag; // タグ
-
->>>>>>> 4aa7962d2502de930d46a2127e2320a71ea786e3
 	public String getUser_id() {
 		return user_id;
 	}
+
 
 	public void setUser_id(String user_id) {
 		this.user_id = user_id;
 	}
 
+
 	public Integer getAmount() {
 		return amount;
 	}
+
 
 	public void setAmount(Integer amount) {
 		this.amount = amount;
 	}
 
+
 	public String getEmotion() {
 		return emotion;
 	}
+
 
 	public void setEmotion(String emotion) {
 		this.emotion = emotion;
 	}
 
+
 	public String getCategory() {
 		return category;
 	}
 
+
 	public void setCategory(String category) {
 		this.category = category;
-<<<<<<< HEAD
 	}
 
-=======
-	}	
-	
->>>>>>> 4aa7962d2502de930d46a2127e2320a71ea786e3
+
 	public String getSituation() {
 		return situation;
 	}
+
 
 	public void setSituation(String situation) {
 		this.situation = situation;
 	}
 
+
 	public String getItem_name() {
 		return item_name;
 	}
+
 
 	public void setItem_name(String item_name) {
 		this.item_name = item_name;
 	}
 
+
 	public String getMemo() {
 		return memo;
 	}
+
 
 	public void setMemo(String memo) {
 		this.memo = memo;
 	}
 
+
 	public String getCreated_at() {
 		return created_at;
 	}
 
+
 	public void setCreated_at(String created_at) {
 		this.created_at = created_at;
 	}
-
 	public String getTag() {
 		return tag;
 	}
+
 
 	public void setTag(String tag) {
 		this.tag = tag;
 	}
 
-<<<<<<< HEAD
 	//コンストラクタ
 	public ExpensesDto(Integer id, String user_id, Integer amount, String emotion, String category, String situation,
-=======
-	public ExpensesDto(String user_id, Integer amount, String emotion, String category, String situation,
->>>>>>> 4aa7962d2502de930d46a2127e2320a71ea786e3
 			String item_name, String memo, String created_at, String tag) {
 		super();
 		this.id = id;
@@ -133,12 +127,10 @@ public class ExpensesDto implements Serializable {
 		this.tag = tag;
 	}
 
+
 	public ExpensesDto() {
 		this.user_id = "";
-<<<<<<< HEAD
 		this.id = 0;
-=======
->>>>>>> 4aa7962d2502de930d46a2127e2320a71ea786e3
 		this.amount = 0;
 		this.emotion = "";
 		this.category = "";
@@ -150,3 +142,4 @@ public class ExpensesDto implements Serializable {
 	}
 
 }
+
